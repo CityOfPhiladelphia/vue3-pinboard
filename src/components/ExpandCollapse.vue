ab1297c717ce1ba1d6335071dc4bf84560b81a48
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2024-10-25 14:28:28 -0400

    imports vue-router functions

--- src/components/ExpandCollapse.vue
+++ src/components/ExpandCollapse.vue
@@ -5,6 +5,8 @@ import { useMapStore } from '../stores/MapStore.js';
 import { useGeocodeStore } from '../stores/GeocodeStore.js';
 import { useDataStore } from '../stores/DataStore.js';
 import { useConfigStore } from '../stores/ConfigStore.js';
+import { useRoute, useRouter } from 'vue-router';
+import { ref, computed, getCurrentInstance, onMounted, watch } from 'vue';
 
 // import { library } from '@fortawesome/fontawesome-svg-core';
 // import { findIconDefinition } from '@fortawesome/fontawesome-svg-core';
