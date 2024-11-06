3c6fcb6b277c380b36435c8346bb484298aeaebb
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2024-11-06 13:03:29 -0500

    testing release dist folder

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,15 @@ import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  },
   css: {
     preprocessorOptions: {
       scss: {
