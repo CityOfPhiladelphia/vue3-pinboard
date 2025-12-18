8cc3f704d7c07b35407011b55b511db3f645acea
Theron Halsey <theron.halsey@phila.gov>
2025-12-18 09:55:52 -0500

    simplified useExternalModule

--- src/components/map/CyclomediaPanel.vue
+++ src/components/map/CyclomediaPanel.vue
@@ -158,6 +158,7 @@ const setNewLocation = async (coords) => {
       if (import.meta.env.VITE_DEBUG) console.log('update cyclomedia date, viewer.props.recording.year:', viewer.props.recording.year);
       MapStore.cyclomediaYear = viewer.props.recording.year;
       const orientation = viewer.getOrientation();
+      viewer.setOrientation({ pitch: 0 });
       if (import.meta.env.VITE_DEBUG) console.log('orientation:', orientation);
       if (viewer.props.orientation.xyz !== MapStore.cyclomediaCameraXyz) {
         const lngLat = [ viewer.props.orientation.xyz[0], viewer.props.orientation.xyz[1] ];
--- src/composables/cyclomedia/useExternalModule.js
+++ src/composables/cyclomedia/useExternalModule.js
@@ -1,6 +1,9 @@
 import { loadScript } from "./loadScript";
 
 /**
+ * Takes an array of Objects hwere each Object contains the script's source and attributes
+ * It will then attempt to download and append each script to the document head
+ * Promise will resolve to true if all scripts are sucessfully loaded
  *
  * @param {Array[Object]} scripts
  * @returns {Promise(Boolean)}
@@ -8,15 +11,7 @@ import { loadScript } from "./loadScript";
 
 export const useExternalModule = async (scripts) => {
   return new Promise((resolve, reject) => {
-    let allLoaded = true;
-    scripts.forEach(async (script) => {
-      try {
-        await loadScript(script.src, script?.type ?? 'text/javascript', script?.integrity ?? '', script?.crossorigin ?? '', script?.async ?? false, script?.defer ?? false);
-      } catch (error) {
-        allLoaded = false;
-        console.error(error);
-      }
-    });
-    allLoaded ? resolve(allLoaded) : reject(new Error('Failed to load all external scripts'))
+    const allLoaded = Promise.all(Array.from(scripts, (script) => loadScript(script.src, script?.type ?? 'text/javascript', script?.integrity ?? '', script?.crossorigin ?? '', script?.async ?? false, script?.defer ?? false)))
+    allLoaded.state === "fulfilled" ? resolve(true) : reject(new Error('Failed to load all external scripts'))
   })
 };
